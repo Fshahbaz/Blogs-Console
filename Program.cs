9512537998191af26cf506757da3a51761517512
fshahbaz@my.wctc.edu <fshahbaz@my.wctc.edu>
2019-11-04 16:00:53 -0600

    Console blog 2

--- Program.cs
+++ Program.cs
@@ -11,66 +11,159 @@ namespace BlogsConsole
         private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public static void Main(string[] args)
         {
+            Boolean programRunning = true;
             logger.Info("Program started");
             try
             {
-
-                Console.WriteLine("1) Display All Blogs");
-                Console.WriteLine("2) Add Blogs");
-                Console.WriteLine("3) Create the Post");
-
-                string choice = Console.ReadLine();
-
-                BloggingContext db = new BloggingContext();
-
-                if (choice == "1")
+                while (programRunning)
                 {
-                    // Display all Blogs from the database
-                    IOrderedQueryable<Blog> query = db.Blogs.OrderBy(b => b.Name);
+                    Console.WriteLine("1) Display All Blogs");
+                    Console.WriteLine("2) Add Blogs");
+                    Console.WriteLine("3) Create the Post");
+                    Console.WriteLine("4) Display Posts");
+                    Console.WriteLine("Enter q to quit");
 
-                    Console.WriteLine("All blogs in the database:");
-                    foreach (Blog item in query)
+                    string choice = Console.ReadLine();
+
+                    BloggingContext db = new BloggingContext();
+
+                    Console.Clear();
+                    if (choice == "1")
                     {
-                        Console.WriteLine(item.Name);
-
-                    }
-                } else if (choice == "2")
-                {
-                    // Create and save a new Blog
-                    Console.Write("Enter a name for a new Blog: ");
-                    string name = Console.ReadLine();
-
-                    Blog blog = new Blog { Name = name };
-
-                    db.AddBlog(blog);
-                    logger.Info("Blog added - {name}", name);
-                } else if (choice == "3")
-                {
-                    Console.WriteLine("Enter the name of the Blog you are posting to: ");
-                    Boolean blogExists = false;
-                    string userBlog = Console.ReadLine();
-                    IOrderedQueryable<Blog> query = db.Blogs.OrderBy(b => b.Name);
-                    Blog blog = new Blog();
-                    foreach (Blog item in query)
-                    {
-                        if (item.Name == userBlog)
+                        logger.Info("Option 1 selected");
+                        // Display all Blogs from the database
+                        IOrderedQueryable<Blog> query = db.Blogs.OrderBy(b => b.Name);
+                        int blogCount = db.Blogs.Count();
+                        Console.WriteLine(blogCount + " Blogs returned");
+                        foreach (Blog item in query)
                         {
-                            blogExists = true;
-                            blog = item;
+                            Console.WriteLine(item.Name);
+                        }
+                        Console.WriteLine();
+                    }
+                    else if (choice == "2")
+                    {
+                        logger.Info("Option 2 selected");
+                        // Create and save a new Blog
+                        Console.Write("Enter a name for a new Blog: ");
+
+                        string name = Console.ReadLine();
+
+                        if (name.Length == 0)
+                        {
+                            logger.Info("Blog name cannot be null");
+                        }
+                        else
+                        {
+                            Blog blog = new Blog { Name = name };
+                            db.AddBlog(blog);
+                            logger.Info("Blog added - {name}", name);
+                        }
+                        Console.WriteLine();
+                    }
+                    else if (choice == "3")
+                    {
+                        logger.Info("Option 3 selected");
+                        int blogCount = db.Blogs.Count();
+                        if (blogCount > 0) {
+                            Console.WriteLine("Select the blog you would like to post to: ");
+                            IOrderedQueryable<Blog> query = db.Blogs.OrderBy(b => b.BlogId);
+                            foreach (Blog item in query)
+                            {
+                                Console.WriteLine(item.BlogId+")" + " " + item.Name);
+                            }
+                            string userOption = Console.ReadLine();
+                            int val = 0;
+                            if (!(int.TryParse(userOption, out val)))
+                            {
+                                logger.Info("Invalid Blog Id");
+                            }
+                            else 
+                            {
+                                int id = int.Parse(userOption);
+                                int blogIDMatchCount = query.Where(b => b.BlogId.Equals(id)).Count();
+                                if (blogIDMatchCount > 0)
+                                {
+                                    Console.WriteLine("Enter the Post Title: ");
+                                    string title = Console.ReadLine();
+                                    if (title.Length == 0)
+                                    {
+                                        logger.Info("Post title cannot be null");
+                                    } else
+                                    {
+                                        Console.WriteLine("Enter the Post Content: ");
+                                        string content = Console.ReadLine();
+                                        Post post = new Post { Title = title, Content = content, BlogId = id };
+                                        logger.Info("Post added - {title}", title);
+                                        db.AddPost(post);
+                                    }
+                                }
+                                else
+                                {
+                                    logger.Info("There are no Blogs saved with that Id");
+                                }
+                            }
+                        } else
+                        {
+                            logger.Info("No blogs found. Use option 2 to add a blog.");
+                        }
+                        Console.WriteLine();
+                    }
+                    else if (choice == "4")
+                    {
+
+                        logger.Info("Option 4 selected");
+                        Console.WriteLine("Select the blog's posts to display: ");
+                        IOrderedQueryable<Blog> blogQuery = db.Blogs.OrderBy(b => b.BlogId);
+                        Console.WriteLine("0) Posts from all blogs");
+                        foreach (Blog item in blogQuery)
+                        {
+                            Console.WriteLine(item.BlogId + ")" + " Posts from " + item.Name);
+                        }
+                        string userOption = Console.ReadLine();
+                        if (userOption == "0")
+                        {
+                            int postCount = db.Posts.Count();
+                            Console.WriteLine(postCount + " post(s) returned");
+                            IOrderedQueryable<Post> postQuery = db.Posts.OrderBy(b => b.PostId);
+                            foreach (Post item in postQuery)
+                            {
+                                Console.WriteLine("Blog: " + item.Blog.Name);
+                                Console.WriteLine("Title: " + item.Title);
+                                Console.WriteLine("Content: " + item.Content);
+                                Console.WriteLine();
+                            }
+                        }
+                        int val = 0;
+                        if ((int.TryParse(userOption, out val)))
+                        {
+                            int optionInt = int.Parse(userOption);
+                            IQueryable<Post> postQuery = db.Posts.Where(p => p.BlogId.Equals(optionInt));
+                            int postCount = postQuery.Count();
+                            Console.WriteLine(postCount + " post(s) returned");
+                            if (postCount > 0)
+                            {
+                                foreach (Post item in postQuery)
+                                {
+                                    Console.WriteLine("Blog: " + item.Blog.Name);
+                                    Console.WriteLine("Title: " + item.Title);
+                                    Console.WriteLine("Content: " + item.Content);
+                                    Console.WriteLine();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            logger.Info("Invalid Blog Id");
                         }
                     }
-                    if (blogExists == true)
+                    else if (choice == "q")
                     {
-                        Console.WriteLine("Enter the Post Title: ");
-                        string title = Console.ReadLine();
-                        Console.WriteLine("Enter the Post Content: ");
-                        string content = Console.ReadLine();
-                        Post post = new Post { Title = title, Content = content, Blog = blog };
-                        logger.Info("Post added - {title}", title);
-                        db.AddPost(post);
-                    } else
+                        programRunning = false;
+                    }
+                    else
                     {
-                        Console.WriteLine("ERROR: Entered blog does not exsist.");
+                        logger.Info("Invalid option");
                     }
                 }
             }
